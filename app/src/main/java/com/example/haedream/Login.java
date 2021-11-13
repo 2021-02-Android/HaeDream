243d1fb2a4439cbb69cd2ce69204d3e6f1a4ae3a
1DOX <veritas0818@gmail.com>
2021-11-14 03:36:18 +0900

    데이터베이스 연결 테스트 php연결부에서 에러남...ㅜㅜ

--- app/src/main/java/com/example/haedream/Login.java
+++ app/src/main/java/com/example/haedream/Login.java
@@ -3,6 +3,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,11 +27,15 @@ public class Login extends AppCompatActivity {
 
         // 로그인 버튼 누를 시 메인 화면으로 이동
         Button loginButton = (Button) findViewById(R.id.login);
+        EditText user_id = findViewById(R.id.user_id);
+        EditText user_pw = findViewById(R.id.user_pw);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainSCR.class);
-                startActivity(intent);
+                Intent loginintent = new Intent(getApplicationContext(), LoginActivity.class);
+                loginintent.putExtra("user_id", user_id.getText().toString());
+                loginintent.putExtra("user_pw", user_pw.getText().toString());
+                startActivity(loginintent);
             }
         });
     }
--- app/src/main/java/com/example/haedream/LoginActivity.java
+++ app/src/main/java/com/example/haedream/LoginActivity.java
@@ -18,8 +18,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText user_id, user_pw;
-    private Button btn_login, btn_join;
+    private String user_id, user_pw;
+    private boolean isLogin = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //액티비티 시작시 처음으로 실행되는 생명주기
@@ -27,61 +27,46 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        user_id = findViewById(R.id.user_id);
-        user_pw = findViewById(R.id.user_pw);
-        btn_join = findViewById(R.id.go_join);
-        btn_login = findViewById(R.id.login);
+        Intent loginIntent = getIntent();
+        user_id = loginIntent.getStringExtra("user_id");
+        user_pw = loginIntent.getStringExtra("user_pw");
+        Log.d("[TAG] 로그인 디버깅", "인텐트 받아옴: " + user_id + user_pw);
 
-        // 회원가입 버튼 클릭 시 수행
-        /*btn_join.setOnClickListener(new View.OnClickListener() {
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });*/
+            public void onResponse(String response) {
+                try {
+                    // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
+                    //System.out.println("hongchul" + response);
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    if (success) { // 로그인에 성공한 경우
+                        String userID = jsonObject.getString("userID");
+                        String userPW = jsonObject.getString("userPW");
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 사용자가 EditText에 입력한 값을 가져옴
-                String userID = user_id.getText().toString(); //ID
-                String userPW = user_pw.getText().toString(); //PW
-
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-                            System.out.println("hongchul" + response);
-                            JSONObject jsonObject = new JSONObject(response);
-                            boolean success = jsonObject.getBoolean("success");
-                            if (success) { // 로그인에 성공한 경우
-                                String userID = jsonObject.getString("userID");
-                                String userPW = jsonObject.getString("userPW");
-
-                                Toast.makeText(getApplicationContext(),"로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
-                                Log.d("[TAG] 로그인 디버깅", "로그인 성공");
-                                //Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                //intent.putExtra("userID", userID);
-                                //intent.putExtra("userPW", userPW);
-                                //startActivity(intent);
-                            } else { // 로그인에 실패한 경우
-                                Toast.makeText(getApplicationContext(),"로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
-                                Log.d("[TAG] 로그인 디버깅", "로그인 실패");
-                                return;
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                        Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                        Log.d("[TAG] 로그인 디버깅", "로그인 성공");
+                        isLogin = true;
+                    } else { // 로그인에 실패한 경우
+                        Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                        Log.d("[TAG] 로그인 디버깅", "로그인 실패");
+                        return;
                     }
-                };
-                LoginRequest loginRequest = new LoginRequest(userID, userPW, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-                queue.add(loginRequest);
+                } catch (JSONException e) {
+                    Log.d("[TAG] 로그인 디버깅", "catch: 연결 실패");
+                    e.printStackTrace();
+                }
             }
-        });
+        };
+        LoginRequest loginRequest = new LoginRequest(user_id, user_pw, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(loginRequest);
 
-
+        if (isLogin) {
+            Intent intent = new Intent(getApplicationContext(), MainSCR.class);
+            startActivity(intent);
+        } else {
+            Log.d("[TAG] 로그인 디버깅", "로그인 실패2");
+        }
     }
 }
