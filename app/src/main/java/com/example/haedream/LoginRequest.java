1d1fde5c0044af8fe157a42a3487ffece8f85ba3
kimhyoju <qjqmf217@naver.com>
2021-11-14 20:09:41 +0900

    심부름 요청 화면 id 변경, 심부름 요청 시 디비에 저장 구현

--- app/src/main/java/com/example/haedream/LoginRequest.java
+++ app/src/main/java/com/example/haedream/LoginRequest.java
@@ -9,10 +9,9 @@ import java.util.Map;
 
 public class LoginRequest extends StringRequest {
 
-    // 서버 URL 설정 (PHP 파일 연동 - Database 바로 접근 불가, php 중간 매체로 파싱하여 사용)
-    // php 파일은 호스팅서버 www 폴더 아래에 위치 (폴더 변경X)
-    final static private String URL = "http://idox23.cafe24.com/Login.php"; // php URL 지정
-    private Map<String, String> map; // 여기부터 php 파일 파싱하여 사용하기 위한 소스 작성
+    // 서버 URL 설정 ( PHP 파일 연동 - Database 바로 접근 불가, php 중간 매체로 파싱하여 사용 )
+    final static private String URL = "http://idox23.cafe24.com/Login.php";
+    private Map<String, String> map;
 
 
     public LoginRequest(String userID, String userPW, Response.Listener<String> listener) {
