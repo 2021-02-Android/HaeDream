2c780f128727f4c66abb10a3a2586d80822a1c4f
kimhyoju <qjqmf217@naver.com>
2021-11-23 00:28:43 +0900

    심부름 요청 후 리스트에 표시

--- app/src/main/java/com/example/haedream/SimhelpList.java
+++ app/src/main/java/com/example/haedream/SimhelpList.java
@@ -1,29 +1,37 @@
 package com.example.haedream;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class SimhelpList extends AppCompatActivity {
+    ListView listView;
+    ArrayList<simhelpitem> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.helplist);
 
-        ListView listView = findViewById(R.id.listview);
-        SimhelpAdapter adapter = new SimhelpAdapter();
-        adapter.addItem(new simhelpitem("변민정", "이삿짐 | 가야1치안", "5분 거리 이사 도와주실분 구합니다", "5000p", R.drawable.move1));
-        adapter.addItem(new simhelpitem("박경민", "청소 | 연제구", "설거지", "1000p", R.drawable.clean1));
-        listView.setAdapter(adapter);
+        listView = findViewById(R.id.listview);
+        arrayList = new ArrayList<>();
+
+        new SimhelpList.Select_HelpList_Request().execute();
 
         ImageButton simButton = (ImageButton) findViewById(R.id.callbutton);
         simButton.setOnClickListener(new View.OnClickListener() {
@@ -35,42 +43,60 @@ public class SimhelpList extends AppCompatActivity {
         });
     }
 
-    public class SimhelpAdapter extends BaseAdapter {
-        ArrayList<simhelpitem> items = new ArrayList<simhelpitem>();
+    class Select_HelpList_Request extends AsyncTask<String, Integer, String> {
+        String result = null;
         @Override
-        public int getCount() {
-            return items.size();
-        }
+        protected String doInBackground(String... rurls) {
+            try {
+                URL url = new URL("https://idox23.cafe24.com/task_result.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
 
-        public void addItem(simhelpitem item){
-            items.add(item);
-        }
-        @Override
-        public Object getItem(int position) {
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            SimhelpItemView simhelpItemView = null;
-
-            if(convertView == null){
-                simhelpItemView = new SimhelpItemView(getApplicationContext());
-            } else {
-                simhelpItemView = (SimhelpItemView)convertView;
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    result = stringBuilder.toString();
+                } else {
+                    result = "error";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            simhelpitem item = items.get(position);
-            simhelpItemView.setName(item.getName());
-            simhelpItemView.setSub(item.getSub());
-            simhelpItemView.setSubstring(item.getSubstring());
-            simhelpItemView.setPoint(item.getPoint());
-            simhelpItemView.setImage(item.getResid());
-            return simhelpItemView;
+            return result;
+        }
+
+        protected void onPostExecute(String _result) {
+            try {
+                JSONObject root = new JSONObject(_result);
+                JSONArray results = new JSONArray(root.getString("results"));
+
+                for (int index = 0; index < results.length(); index++) {
+                    JSONObject Content = results.getJSONObject(index);
+                    String category = Content.getString("category");
+                    String details = Content.getString("details");
+                    String location = Content.getString("location");
+                    String info = Content.getString("info");
+                    String point = Content.getString("point");
+
+                    simhelpitem item = new simhelpitem();
+
+                    item.setCategory(category);
+                    item.setDetails(details);
+                    item.setLocation(location);
+                    item.setInfo(info);
+                    item.setPoint(point);
+                    arrayList.add(item);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            SimhelpItemAdapter helpListViewAdapter = new SimhelpItemAdapter(SimhelpList.this, arrayList);
+            listView.setAdapter(helpListViewAdapter);
         }
     }
 }
