142019ae4d0171a8644aa51386994303d57fe284
kimhyoju <qjqmf217@naver.com>
2021-12-05 16:09:33 +0900

    12/05 HYOJU 소개해드림리스트뷰

---
+++ app/src/main/java/com/example/haedream/IntroListItem.java
@@ -0,0 +1,13 @@
+package com.example.haedream;
+
+public class IntroListItem {
+    String name, depart;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getDepart() { return depart; }
+
+    public void setDepart(String depart) { this.depart = depart; }
+}
--- app/src/main/java/com/example/haedream/Intro_List.java
+++ app/src/main/java/com/example/haedream/Intro_List.java
@@ -1,33 +1,57 @@
 package com.example.haedream;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class Intro_List extends AppCompatActivity {
+    ListView listView;
+    ArrayList<IntroListItem> arrayList;
+    String user_id;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduce_list);
 
-        String user_id;
         Intent userintent = getIntent();
         user_id = userintent.getStringExtra("user_id");
         Log.d("[user_id 인텐트 받아옴]", user_id);
+
+        listView = findViewById(R.id.intro_listview);
+        arrayList = new ArrayList<>();
+
+        new Intro_List.Select_HelpList_Request().execute();
+
 
         // 말풍선 버튼 누를 시 이동
         ImageButton list = (ImageButton) findViewById(R.id.list_btn);
         list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // ConvertList.class로 변경해야함 ((추가예정))
                 Intent intent = new Intent(getApplicationContext(), Intro_List.class);
+                intent.putExtra("user_id", user_id);
                 startActivity(intent);
+                finish();
             }
         });
 
@@ -65,15 +89,57 @@ public class Intro_List extends AppCompatActivity {
             }
         });*/
 
-        // 소개 버튼 누를 시 이동
-        ImageButton intro = (ImageButton) findViewById(R.id.go_intro);
-        intro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Intro_List.class);
-                startActivity(intent);
-            }
-        });
+    }
 
+    class Select_HelpList_Request extends AsyncTask<String, Integer, String> {
+        String result = null;
+        @Override
+        protected String doInBackground(String... rurls) {
+            try {
+                URL url = new URL("https://idox23.cafe24.com/user_result.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    result = stringBuilder.toString();
+                } else {
+                    result = "error";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String _result) {
+            try {
+                JSONObject root = new JSONObject(_result);
+                JSONArray results = new JSONArray(root.getString("results"));
+
+                for (int index = 0; index < results.length(); index++) {
+                    JSONObject Content = results.getJSONObject(index);
+                    String userid = Content.getString("name");
+                    String depart = Content.getString("tel");
+
+                    IntroListItem item = new IntroListItem();
+
+                    item.setName(userid);
+                    item.setDepart(depart);
+                    arrayList.add(item);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            IntroListAdapter introListAdapter = new IntroListAdapter(Intro_List.this, arrayList);
+            listView.setAdapter(introListAdapter);
+        }
     }
 }
