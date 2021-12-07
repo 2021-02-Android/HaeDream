501035188004e0f67c52807b6b0d0ae98219d488
1DOX <veritas0818@gmail.com>
2021-12-07 23:53:08 +0900

    카카오 로그인 오류 수정 2 진짜 수정 또수정

--- app/src/main/java/com/example/haedream/KakaoLogin.java
+++ app/src/main/java/com/example/haedream/KakaoLogin.java
@@ -154,7 +154,7 @@ public class KakaoLogin extends AppCompatActivity {
 
     public class JoinRequest extends StringRequest {
         // 서버 URL 설정 ( PHP 파일 연동 - Database 바로 접근 불가, php 중간 매체로 파싱하여 사용 )
-        final static private String URL = "http://idox23.cafe24.com/Join.php";
+        final static private String URL = "http://idox23.cafe24.com/id_overlap.php";
         private Map<String, String> map;
 
         public JoinRequest(String userID, Response.Listener<String> listener) {
