1a2f68b510fe50f1d4e534c6f0d57a15d77baa27
kimhyoju <qjqmf217@naver.com>
2021-12-08 03:22:19 +0900

    12/08 profile

--- app/src/main/java/com/example/haedream/SimAccept.java
+++ app/src/main/java/com/example/haedream/SimAccept.java
@@ -1,6 +1,7 @@
 package com.example.haedream;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,8 +11,23 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SimAccept extends AppCompatActivity {
     TextView name, location, info, point;
+    CircleImageView iv_profile;
+    String it_userid;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,11 +47,14 @@ public class SimAccept extends AppCompatActivity {
          location = findViewById(R.id.sub);
          info = findViewById(R.id.substring);
          point = findViewById(R.id.point);
+         iv_profile = findViewById(R.id.iv);
 
          name.setText(it_name);
          location.setText(it_location);
          info.setText(it_info);
          point.setText(it_point);
+
+        new SimAccept.Select_SimAccept_Request().execute();
 
          // 취소 버튼 클릭 시
          Button cancelbtn = findViewById(R.id.cancelbtn);
@@ -68,4 +87,54 @@ public class SimAccept extends AppCompatActivity {
         });
 
     }
+
+    class Select_SimAccept_Request extends AsyncTask<String, Integer, String> {
+        String result = null;
+        @Override
+        protected String doInBackground(String... rurls) {
+            try {
+                URL url = new URL("https://idox23.cafe24.com/infojoinuser_result.php");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.connect();
+
+                if(conn.getResponseCode()==HttpURLConnection.HTTP_OK) {
+                    InputStreamReader inputStreamReader = new InputStreamReader(conn.getInputStream());
+                    BufferedReader reader = new BufferedReader(inputStreamReader);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    result = stringBuilder.toString();
+                } else {
+                    result = "error";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        protected void onPostExecute(String _result) {
+            try {
+                JSONObject root = new JSONObject(_result);
+                JSONArray results = new JSONArray(root.getString("results"));
+
+                for (int index = 0; index < results.length(); index++) {
+                    JSONObject Content = results.getJSONObject(index);
+                    String uname = Content.getString("name");
+                    String profile = Content.getString("profile");
+
+                    if (name.getText().toString().equals(uname)) {
+                        Glide.with(iv_profile).load("https://idox23.cafe24.com/"+profile).into(iv_profile);
+                        Log.d("이미지 경로 : ", profile);
+                    }
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
